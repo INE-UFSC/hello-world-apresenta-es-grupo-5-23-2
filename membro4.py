218984f3ddd6558907bd9c2dab07e13d00bda5ba
nuvme <pedrofmm13@gmail.com>
2023-08-10 14:53:55 -0300

    add new member in app.py and att hello in membro4

---
+++ membro4.py
@@ -0,0 +1,8 @@
+def hello():
+    apr = """Olá, eu sou o Pedro, tenho 18 anos. 
+Meus hobbies favoritos são ler, jogar bola e também gosto muito de nadar (mal).
+Tenho um curso de formação como Desenvolvedor Python, pelo Entra21 (iniciativa blumenause e agora expandida pelo governo do estado), 
+tive contato com outras linguagens e paradigmas durante o Ensino Médio Técnico e também na minha área de trabalho.
+Nessa disciplina espero aprofundar meus conhecimentos em POO, principalmente com base nas boas práticas e SOLID
+    """
+    return apr
