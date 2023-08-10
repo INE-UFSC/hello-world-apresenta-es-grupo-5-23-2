ab2cea185983bc905e5d34452409c2a29927b23f
Jucian <jucian_decezare2015@hotmail.com>
2023-08-10 15:26:51 -0300

    Apresentação pessoal

--- membro3.py
+++ membro3.py
@@ -1,2 +1,4 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, me chamo Jucian, tenho 18 anos, sou jogador de Xadrez e curto andar de bicicleta, minha experiência prévia em programação se resume ao ensino médio técnico em informática para internet, alguns projetos pessoais e tentivas falhas de criar uma startup." 
+
+#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
